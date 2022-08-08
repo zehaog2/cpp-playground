3403848597b911af56ecdcfc91bc5414906dc0da
SAYO <73139217+zehaog2@users.noreply.github.com>
2022-08-08 18:59:14 -0500

    Update Containter.cpp

--- Containter.cpp
+++ Containter.cpp
@@ -1,10 +1,3 @@
-/* This program serves specifically to help myself understand and practice c++. 
-In the console, user can play around with the multimap in many ways.
-The most complicated search program's effectiveness (narrow down upon a guessed number to possible
-numbers in data) can definitely be improved.
-The efficiency of the 8 different MENU methods can be improved.
-The user interface can be improved to HTML format. 
-*/
 #include <iostream>
 #include <queue>
 #include <string> 
